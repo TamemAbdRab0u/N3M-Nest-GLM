b1cd44c6338e835252d63f01b9e05f6d87398df9
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-11-23 09:14:13 +0200

    feat: jwt section and class added , required packages added

---
+++ GLM.BL/Helper/Jwt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.Helper
+{
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string issuer { get; set; }
+        public string Audience { get; set; }
+        public string DurationInDays { get; set; }
+    }
+}
