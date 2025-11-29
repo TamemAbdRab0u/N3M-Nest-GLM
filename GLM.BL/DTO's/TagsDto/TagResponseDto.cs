39706d092c30e4f261834a2519e4064b670c9cc4
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-11-29 08:41:02 +0200

    feat: Added Tags Dto's

---
+++ GLM.BL/DTO's/TagsDto/TagResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.DTO_s.TagsDto
+{
+    public class TagResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
