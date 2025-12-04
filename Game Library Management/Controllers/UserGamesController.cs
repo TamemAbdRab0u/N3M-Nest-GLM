8cd7cc202a6f1dba05e96549e80e8e1fb9b6e41d
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-12-04 14:32:47 +0200

    feat: UserGames Controller Documentation

--- Game Library Management/Controllers/UserGamesController.cs
+++ Game Library Management/Controllers/UserGamesController.cs
@@ -17,6 +17,9 @@ namespace Game_Library_Management.Controllers
             this.usergameService = usergameService;
         }
 
+        /// <summary>
+        /// Return All User Games.
+        /// </summary>
         [HttpGet("GetAllUserGames")]
         public async Task<IActionResult> GetAllUserGames()
         {
@@ -31,6 +34,10 @@ namespace Game_Library_Management.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Return a Single User Game.
+        /// </summary>
+        /// <param name="gameId">Game Id</param>
         [HttpGet("GetUserGameById/{gameId}")]
         public async Task<IActionResult> GetUserGameById([FromRoute] int gameId)
         {
@@ -45,6 +52,10 @@ namespace Game_Library_Management.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Create New User Game.
+        /// </summary>
+        /// <param name="createDto">Game Information</param>
         [HttpPost("CreateUserGame")]
         public async Task<IActionResult> CreateUserGame([FromBody] UserGamesCreateDto createDto)
         {
@@ -65,6 +76,10 @@ namespace Game_Library_Management.Controllers
             }
         }
 
+        /// <summary>
+        /// Update an Existing User Game.
+        /// </summary>
+        /// <param name="updateDto">Game Information</param>
         [HttpPatch("UpdateUserGame")]
         public async Task<IActionResult> UpdateUserGame([FromBody] UserGamesCreateDto updateDto)
         {
@@ -79,6 +94,10 @@ namespace Game_Library_Management.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Remove a User Game.
+        /// </summary>
+        /// <param name="GameId">Game Id</param>
         [HttpDelete("DeleteUserGame/{gameId}")]
         public async Task<IActionResult> DeleteUserGame([FromRoute]int GameId)
         {
