750ff89528b2d75d75c5e31a455ed287c18a493d
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-12-04 14:37:50 +0200

    fix: Making The GameState To Be Returned as String Instead OF the Numbers

--- GLM.BL/DTO's/UserGamesDto/UserGamesResponseDto.cs
+++ GLM.BL/DTO's/UserGamesDto/UserGamesResponseDto.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Game_Library_Management_BL.DTO_s.UserGamesDto
