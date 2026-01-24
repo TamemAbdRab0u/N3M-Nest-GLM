f149c6c4bd99d72fb7be38e6935cdcb8a6f68542
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-24 11:47:42 +0200

    feat: Games Stats Services

---
+++ GLM.BL/Services/IServices/IStatsService.cs
@@ -0,0 +1,17 @@
+﻿using Game_Library_Management_BL.DTO_s.Stats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.Services.IServices
+{
+    public interface IStatsService
+    {
+        Task<IEnumerable<GamesStatsDto>> CompletedGamesAsync(string userId);
+        Task<IEnumerable<GamesStatsDto>> PlayingGamesAsync(string userId);
+        Task<IEnumerable<GamesStatsDto>> WhishlistedGamesAsync(string userId);
+        Task<IEnumerable<GamesStatsDto>> DroppedGamesAsync(string userId);
+    }
+}
