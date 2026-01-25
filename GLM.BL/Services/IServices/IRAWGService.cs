2d311ffcb2bd3059707d04a2ec5021f754a3ff36
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-25 09:35:37 +0200

    feat: RAWG Search Services

---
+++ GLM.BL/Services/IServices/IRAWGService.cs
@@ -0,0 +1,14 @@
+﻿using Game_Library_Management_BL.DTO_s.RAWGDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.Services.IServices
+{
+    public interface IRAWGService
+    {
+        Task<IEnumerable<RAWGCatalogDto>> SearchGamesAsync(string query);
+    }
+}
---
+++ GLM.BL/Services/Services/RAWGService.cs
@@ -0,0 +1,44 @@
+﻿using Game_Library_Management_BL.DTO_s.RAWGDto;
+using Game_Library_Management_BL.Services.IServices;
+using Game_Library_Management_BL.UnitOfWork;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.Services.Services
+{
+    public class RAWGService : IRAWGService
+    {
+        private readonly HttpClient _http;
+        private readonly IConfiguration _config;
+        private readonly IUnitOfWork unitofwork;
+
+        public RAWGService(HttpClient http, IConfiguration config, IUnitOfWork unitofwork)
+        {
+            _http = http;
+            _config = config;
+            this.unitofwork = unitofwork;
+        }
+
+        public async Task<IEnumerable<RAWGCatalogDto>> SearchGamesAsync(string query)
+        {
+            var key = _config["RAWG:ApiKey"];
+            var url = $"https://api.rawg.io/api/games?key={key}&search={query}&page_size=20";
+
+            var response = await _http.GetFromJsonAsync<RAWGResponseDto>(url);
+
+            return response.Results.Select(g => new RAWGCatalogDto
+            {
+                ExternalId = g.Id,
+                Title = g.Name,
+                ImageUrl = g.Background_Image,
+                Rating = g.Rating,
+                ReleaseDate = g.Released
+            });
+        }
+    }
+}
