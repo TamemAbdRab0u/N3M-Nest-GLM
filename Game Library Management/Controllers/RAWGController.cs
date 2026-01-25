21f167f70d95b58fa4fb95f88ecfa5eb58630385
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-25 09:36:00 +0200

    faet: RAWG Search Endpoint

---
+++ Game Library Management/Controllers/RAWGController.cs
@@ -0,0 +1,25 @@
+﻿using Game_Library_Management_BL.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Library_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RAWGController : ControllerBase
+    {
+        private readonly IRAWGService _gameCatalogService;
+        public RAWGController(IRAWGService gameCatalogService)
+        {
+            _gameCatalogService = gameCatalogService;
+        }
+
+        [HttpGet("catalog/search")]
+        public async Task<IActionResult> SearchCatalog(string query)
+        {
+            var games = await _gameCatalogService.SearchGamesAsync(query);
+            return Ok(games);
+        }
+
+    }
+}
