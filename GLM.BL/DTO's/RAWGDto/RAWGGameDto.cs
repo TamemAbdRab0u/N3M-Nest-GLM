01ef5475738b649f79e8b9319c7b3a5de634760a
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-25 09:35:14 +0200

    feat: RAWG Dto's

---
+++ GLM.BL/DTO's/RAWGDto/RAWGGameDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_BL.DTO_s.RAWGDto
+{
+    public class RAWGGameDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Background_Image { get; set; }
+        public double Rating { get; set; }
+        public string Released { get; set; }
+    }
+}
