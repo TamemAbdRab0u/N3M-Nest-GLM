b669c03fcf43fb6519b46f8b5bc3c9c14cc928c2
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-26 09:36:37 +0200

    feat: Community Chat (2)[Added The Messages Table And Added The Migration]

---
+++ GLM.DAL/Migrations/20260126071403_Community.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Game_Library_Management_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Community : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "460a5eb0-426f-4c6b-bd39-f0696324573e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dce5284b-98cd-4a7e-9996-4cbe34d7c795");
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SentAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "d76c3fce-14a5-47bf-96e6-389d4467e085", null, "Admin", "Admin" },
+                    { "ef6d06e8-1300-47d7-be2d-324ddae3d38e", null, "User", "User" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d76c3fce-14a5-47bf-96e6-389d4467e085");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ef6d06e8-1300-47d7-be2d-324ddae3d38e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "460a5eb0-426f-4c6b-bd39-f0696324573e", null, "User", "User" },
+                    { "dce5284b-98cd-4a7e-9996-4cbe34d7c795", null, "Admin", "Admin" }
+                });
+        }
+    }
+}
