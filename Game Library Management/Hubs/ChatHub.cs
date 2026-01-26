d4c768afed7553a9e141dd071d7463f85e9670e1
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-26 09:37:11 +0200

    feat: Community Chat (3)[Created The Hub]

---
+++ Game Library Management/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Game_Library_Management_DAL.Data;
+using Game_Library_Management_DAL.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Game_Library_Management.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly AppDbContext context;
+        public ChatHub(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task SendMessage(string message)
+        { 
+            var userId = Context.User?.FindFirst("uid")?.Value ?? "Guest";
+            var UserName = Context.User?.Identity?.Name ?? "Guest";
+
+            var msg = new Message
+            {
+                SenderId = userId,
+                SenderName = UserName,
+                Content = message,
+                SentAt = DateTime.UtcNow
+            };
+
+            await context.Messages.AddAsync(msg);
+            await context.SaveChangesAsync();
+
+            await Clients.All.SendAsync("ReceiveMessage",msg.SenderName,msg.Content,msg.SentAt);
+        }
+    }
+}
