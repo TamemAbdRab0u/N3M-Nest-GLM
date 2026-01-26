c3aadde76318fe19800155dc64e9bba46058f607
Tamem Abdrabou <tamemabdo21@gmail.com>
2026-01-26 09:35:33 +0200

    feat: Community Chat (1)[Created Message Model And Requried Package]

---
+++ GLM.DAL/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Library_Management_DAL.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string SenderName { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
